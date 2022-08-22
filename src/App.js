1c41c06da2afffb434eac48a15b883acafa0968f
Jun <wong.junwei@petronas.com>
2022-08-22 15:29:13 +0800

    Separate button array into config

--- src/App.js
+++ src/App.js
@@ -1,47 +1,62 @@
-import React, {useState} from "react"
-import "./App.css"
-function App () {
-  const [equation, changeEquation] = useState("")
-  const [output, changeOutput] = useState("")
-  const calculatorButtonsArray = ["(",  ")", "<", "AC", "7", "8", "9","/","4","5","6","*","1","2","3","-","0",".","=","+"]
+import React, { useState } from "react";
+import { calculatorButtonsArray } from "./config";
+import "./App.css";
+function App() {
+  const [equation, changeEquation] = useState("");
+  const [output, changeOutput] = useState("");
 
-  const handleOnClick = buttonValue => {
-    switch(buttonValue) {
-      case "AC": changeEquation(""); changeOutput(""); break;
-      case "=": changeOutput(evaluateResults(equation).toString().substr(0,15)); break;
-      case "<": changeEquation(equation.toString().substring(0, equation.length - 1)); break;
-      default: changeEquation((equation+buttonValue).toString().substr(0,15))
+  const handleOnClick = (buttonValue) => {
+    switch (buttonValue) {
+      case "AC":
+        changeEquation("");
+        changeOutput("");
+        break;
+      case "=":
+        changeOutput(evaluateResults(equation).toString().substr(0, 15));
+        break;
+      case "<":
+        changeEquation(equation.toString().substring(0, equation.length - 1));
+        break;
+      default:
+        changeEquation((equation + buttonValue).toString().substr(0, 15));
     }
-  }
+  };
 
-  const evaluateResults = equationString => {
-    let results = ""
-    if(!(equationString === "" || !equation)){
-      for(const i of equationString){
-        if(!calculatorButtonsArray.includes(i))
-          return "HACKER"
+  const evaluateResults = (equationString) => {
+    let results = "";
+    if (!(equationString === "" || !equation)) {
+      for (const i of equationString) {
+        if (!calculatorButtonsArray.includes(i)) return "HACKER";
       }
-      try{
-        results = eval(equationString)
-      }
-      catch{
-        alert("invalid equation")
+      try {
+        results = eval(equationString);
+      } catch {
+        alert("invalid equation");
       }
     }
-    return results
-  }
+    return results;
+  };
 
   return (
     <div className="container">
       <div className="output-panel">
-        <label className="equation">{equation? equation : ""}</label>
-        <label className="results">{output? output  : ""}</label>
+        <label className="equation">{equation ? equation : ""}</label>
+        <label className="results">{output ? output : ""}</label>
       </div>
       <div className="buttons-panel">
-        {calculatorButtonsArray.map(button=><button className="button" key={button} id={`button-${button}`} onClick={event => handleOnClick(button)}>{button}</button>)} 
+        {calculatorButtonsArray.map((button) => (
+          <button
+            className="button"
+            key={button}
+            id={`button-${button}`}
+            onClick={(event) => handleOnClick(button)}
+          >
+            {button}
+          </button>
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
